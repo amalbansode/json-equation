df13f3ea5599adac34aa960a550deab184282f50
Amal <52236912+amalbansode@users.noreply.github.com>
2020-12-26 11:46:54 -0800

    Trying to understand Catch2, incomplete test

---
+++ test/json_equation_test.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
+
+#include "catch.hpp"
+#include "../src/json-equation.h"
+
+using namespace std;
+
+TEST_CASE( "Simple Equation Computation", "[equation]" ) {
+  ifstream infile("../test/equation.json");
+  Equation equation(infile);
+
+  REQUIRE( equation.calculate(0) == 0 );
+}
